bb7c358be4b4396a9f9abbf59bfe7ecd9dbfaf53
lyle dickie <lyle.e.dickie@gmail.com>
2019-09-17 20:21:16 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,30 +4,50 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+  url = "http://www.swapi.co/api/people/"
+  all_character_array = get_all_characters(url)
+  char_result = all_character_array.find do |character|
+    character['name'] == character_name
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  # all_character_array.find{|character| character['name'] == character_name}
+  end
+  char_result ? get_film_list(char_result['films']): nil
+end
+
+def get_all_characters(url)
+  all_character_array = []
+  while url do
+    response_hash = get_hash_from_request(url)
+    all_character_array = all_character_array.concat(response_hash['results'])
+    url = response_hash["next"]
+  end
+  return all_character_array
+end
+
+def get_film_list(film_urls)
+  film_urls.map do |film|
+    get_hash_from_request_for(film)
+  end
+end
+
+def get_hash_from_request(url)
+  response_string = RestClient.get(url)
+  JSON.parse(response_string)
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  puts "---------------------------------------------------"
+  films.each do |film|
+    puts "Title: #{film["title"]}"
+    puts "Director: #{film["director"]}"
+    puts "Producer: #{film["producer"]}"
+    puts "Release Date: #{film["release_date"]}"
+    puts "---------------------------------------------------"
+  end
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
-  print_movies(films)
+  # print_movies(films)
+  films ? print_movies(films): puts( "Sorry try again")
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
