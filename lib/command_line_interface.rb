f83357147a5a5058a3b29dfcd1e6da4e9a06d3cd
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-09-17 15:22:56 -0400

    Built SWAPI film fetcher

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,13 @@
 def welcome
   # puts out a welcome message here!
+  puts "Hello and welcome to the Star Wars API Communicator!"
+  puts "May the Force be with you!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp()
+  return input.downcase
 end
+
