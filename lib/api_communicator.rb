7eceea510811ccb31365b0a9c773c3ca29c5bc48
Reina <r.clarkemitchell@gmail.com>
2019-09-18 13:09:49 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,29 +2,51 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
+def get_character_hash_data
+  # told in review that to parse through the different pages, 
+  # told to loop through by concatenating it onto the 'response_string'
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  response_hash["results"]
 end
 
+
+def find_characters_in_api(character_name)
+  #make the web request
+  hash_data = get_character_hash_data()
+  hash_data.find do |character|
+    character['name'].downcase == character_name 
+  end
+end  
+  
+def get_character_movies_from_api(character_name)
+   character = find_characters_in_api(character_name)
+   get_films = character['films']
+end
+
+
+def film_url_convert(film_array)
+  arr1 = []
+  film_array.map do |film_url|
+    response_string = RestClient.get(film_url)
+    response_hash = JSON.parse(response_string)
+    arr1 << response_hash
+  end
+  arr1
+end
+
+
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+   films.each_with_index do |val , index|
+    # can edit method and choose to number based on the order of the numbers from the original hash. 
+    puts "#{index + 1} #{val['title']}"
+   end
 end
 
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
+films_array_url = get_character_movies_from_api(character)
+films_to_print = film_url_convert(films_array_url)
+print_movies(films_to_print)
 end
 
 ## BONUS
